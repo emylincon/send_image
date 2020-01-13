9254d3267375635b844aabb18490386c13f70740
Emeka Ugwuanyi <emylincon@gmail.com>
2020-01-13 21:25:24 +0000

    formatted to taste

---
+++ new/c2.py
@@ -0,0 +1,46 @@
+import base64
+import codecs as c
+
+import socket
+import os
+
+
+def fin2(barr, dfile):  #this returns a image
+    fh = open(dfile, "wb")
+    #fh.write(barr.decode('base64'))
+    fh.write(c.decode(obj=barr, encoding='base64'))
+    fh.close()
+
+
+def client():
+    host = input('Server IP: ').strip()  # The server's hostname or IP address
+    port = 65001        # The port used by the server
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            fname = input('Enter command: ').strip()
+            s.sendall(fname.encode())
+            data = s.recv(1024)
+            l = data.decode()
+            data_ = str.encode('')
+            while len(data_) < int(l):
+                data_ += s.recv(1024)
+            fin2(barr=data_, dfile='new.jpg')
+            print('File received')
+            s.sendall('exit'.encode())
+            s.close()
+
+    except KeyboardInterrupt:
+        print('Programme Terminated')
+
+
+def main():
+    os.system('clear')
+    print("================== Welcome to Client Platform ===================")
+    client()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ new/s2.py
@@ -0,0 +1,61 @@
+import base64
+import codecs as c
+#!/usr/bin/env python3
+
+import socket
+import os
+
+
+def fin1(file):
+    with open(file, "rb") as imageFile:
+        s = base64.b64encode(imageFile.read())
+        return s
+
+
+def ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+def unicast_call():
+    host = '0.0.0.0'
+    port = 65001        # Port to listen on (non-privileged ports are > 1023)
+
+    print('Server IP: {}'.format(ip_address()))
+
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, port))
+            while True:
+                s.listen()
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected: ', addr)
+                    while True:
+                        data = conn.recv(1024)
+                        msg = data.decode()
+                        if msg == 'send image':
+                            fname = 'img.jpg'
+                            send = fin1(fname)
+                            length = str(len(send)).encode()
+                            conn.sendall(length)
+                            conn.sendall(send)
+
+                        elif msg.lower() == 'exit':
+                            print('Programme Terminated by Client')
+                            conn.close()
+                            break
+    except Exception as e:
+        print(e)
+
+
+def main():
+    os.system('clear')
+    print('================== Programme Active =====================\n')
+    unicast_call()
+
+
+if __name__ == "__main__":
+    main()
+
